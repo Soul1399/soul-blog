19bc56cd1746d672df06ea510495b9bef4895b9d
Philippe Chambord <sulayman.aaliy@gmail.com>
2022-07-18 00:41:35 +0200

    autocomplete for static grid

--- src/app/shared/components/static-grid/static-grid-cell-autocomplete/static-grid-cell-autocomplete.component.ts
+++ src/app/shared/components/static-grid/static-grid-cell-autocomplete/static-grid-cell-autocomplete.component.ts
@@ -1,18 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatOptionSelectionChange } from '@angular/material/core';
-import { Observable } from 'rxjs';
+import * as _ from 'lodash';
+import { Observable, of, Subject } from 'rxjs';
+import {
+  combineLatest,
+  combineLatestWith,
+  debounceTime,
+  map,
+  mergeWith,
+  switchMap,
+} from 'rxjs/operators';
 import { SelectItem } from 'src/app/shared/models/ui/select-item';
+import { OnChange } from 'src/app/shared/on-change';
 
 @Component({
   selector: 'app-static-grid-cell-autocomplete',
   templateUrl: './static-grid-cell-autocomplete.component.html',
-  styleUrls: ['./static-grid-cell-autocomplete.component.scss']
+  styleUrls: ['./static-grid-cell-autocomplete.component.scss'],
 })
 export class StaticGridCellAutocompleteComponent implements OnInit {
   @Input()
-  options$: Observable<SelectItem[]> | null = null;
+  @OnChange<((text: string) => Observable<SelectItem[]>) | null>(function(this: StaticGridCellAutocompleteComponent, fn, change) {
+    this.setupOptions();
+  })
+  filterFunction: ((text: string) => Observable<SelectItem[]>) | null = null;
   @Input()
-  filteredOptions$: Observable<SelectItem[]> | null = null;
+  @OnChange<number>(function(this: StaticGridCellAutocompleteComponent, delay, change) {
+    this.setupOptions();
+  })
+  filterDelay: number = 0;
   @Input()
   allowChange: boolean = true;
   @Input()
@@ -23,17 +39,55 @@ export class StaticGridCellAutocompleteComponent implements OnInit {
   textValue: string | null = null;
   @Input()
   rawValue: any;
+  @Output()
+  selectionChanged = new EventEmitter<SelectItem>();
 
-  constructor() { }
+  options$: Observable<SelectItem[]> = of([]);
+  filteredOptions$: Observable<SelectItem[]> = of([]);
+  readonly inputText$ = new Subject<string>();
+
+  constructor() {}
+
+  @Input()
+  set availableOptions$(value: SelectItem[] | Observable<SelectItem[]> | null) {
+    if (value instanceof Observable) this.options$ = value;
+    else if (_.isArguments(value)) this.options$ = of(value);
+    this.options$ = of([]);
+    this.setupOptions();
+  }
 
   ngOnInit(): void {
+    this.setupOptions();
   }
 
   onInput(text: string) {
-
+    this.inputText$.next(text);
   }
 
   onOptionSelected(item: MatOptionSelectionChange<SelectItem>) {
+    this.selectionChanged.emit(item.source.value);
+  }
 
+  setupOptions() {
+    this.filteredOptions$ = this.inputText$.pipe(
+      debounceTime(this.filterDelay < 0 ? 0 : this.filterDelay),
+      switchMap(text => {
+        if (_.isFunction(this.filterFunction)) {
+          return of(text).pipe(combineLatestWith(this.filterFunction(text)));
+        }
+        return of([text, new Array<SelectItem>()]);
+      }),
+      combineLatestWith(this.options$),
+      map(([[t, s], options]) => {
+        const results = s as SelectItem[];
+        if (options.length > 0) {
+          const text = ((t as string) || '').toLowerCase().trim();
+          return options.filter(
+           (x) => x.name.toString().toLowerCase().trim().indexOf(text) > -1
+          ).concat(results);
+        }
+        return results;
+      })
+    );
   }
 }
